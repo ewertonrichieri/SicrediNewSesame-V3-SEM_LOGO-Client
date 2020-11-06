61b2723b883d8cc34e153e2599983251640dd87b
ewertonrichieri <contato.ewertonrichieri@gmail.com>
2020-11-06 14:17:51 -0300

    Mensagem informando caso o servidor esteje inativo no momento de clicar no botao solicitar do FacilitiesActivity

--- app/src/main/java/com/anyvision/facekeyexample/activities/logged/FacilitiesActivity.java
+++ app/src/main/java/com/anyvision/facekeyexample/activities/logged/FacilitiesActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,73 +26,81 @@ public class FacilitiesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_facilities);
-        btnVigilante = findViewById(R.id.btnVigilante);
-        btnRecepcionista = findViewById(R.id.btnRecepcionista);
-        btnControladorAcesso = findViewById(R.id.btnControladorAcesso);
-        btnBombeiro = findViewById(R.id.btnBombeiro);
+        try{
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_facilities);
+            btnVigilante = findViewById(R.id.btnVigilante);
+            btnRecepcionista = findViewById(R.id.btnRecepcionista);
+            btnControladorAcesso = findViewById(R.id.btnControladorAcesso);
+            btnBombeiro = findViewById(R.id.btnBombeiro);
 
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        int size = sharedPreferences.getInt("facilitiesDescricaoBtn_size", MODE_PRIVATE);
-        final ArrayList<String> listaFacilitiesDescricaoBtn = new ArrayList<String>(size);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            int size = sharedPreferences.getInt("facilitiesDescricaoBtn_size", MODE_PRIVATE);
+            final ArrayList<String> listaFacilitiesDescricaoBtn = new ArrayList<String>(size);
 
-        for (int i = 0; i < size; i++) {
-            listaFacilitiesDescricaoBtn.add(sharedPreferences.getString("facilitiesDescricaoBtn" + "_" + i, null));
+            for (int i = 0; i < size; i++) {
+                listaFacilitiesDescricaoBtn.add(sharedPreferences.getString("facilitiesDescricaoBtn" + "_" + i, null));
+            }
+
+            if(listaFacilitiesDescricaoBtn.size() > 0){
+            btnVigilante.setText(listaFacilitiesDescricaoBtn.get(0));
+            btnRecepcionista.setText(listaFacilitiesDescricaoBtn.get(1));
+            btnControladorAcesso.setText(listaFacilitiesDescricaoBtn.get(2));
+            btnBombeiro.setText(listaFacilitiesDescricaoBtn.get(3));
+            }
+
+            btnVigilante.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        GetVariables.getInstance().setTxtOpcaoFacilities(Enum.Facilities.VIGILANTE.toString());
+                        FacilitiesControleActivity.startActivity(FacilitiesActivity.this);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            btnRecepcionista.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        GetVariables.getInstance().setTxtOpcaoFacilities(Enum.Facilities.RECEPCIONISTA.toString());
+                        FacilitiesControleActivity.startActivity(FacilitiesActivity.this);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            btnControladorAcesso.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        GetVariables.getInstance().setTxtOpcaoFacilities(Enum.Facilities.CONTROLADOR.toString());
+                        FacilitiesControleActivity.startActivity(FacilitiesActivity.this);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            btnBombeiro.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        GetVariables.getInstance().setTxtOpcaoFacilities(Enum.Facilities.BOMBEIRO.toString());
+                        FacilitiesControleActivity.startActivity(FacilitiesActivity.this);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        catch (Exception e){
+            e.printStackTrace();
         }
 
-        btnVigilante.setText(listaFacilitiesDescricaoBtn.get(0));
-        btnRecepcionista.setText(listaFacilitiesDescricaoBtn.get(1));
-        btnControladorAcesso.setText(listaFacilitiesDescricaoBtn.get(2));
-        btnBombeiro.setText(listaFacilitiesDescricaoBtn.get(3));
-
-        btnVigilante.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    GetVariables.getInstance().setTxtOpcaoFacilities(Enum.Facilities.VIGILANTE.toString());
-                    FacilitiesControleActivity.startActivity(FacilitiesActivity.this);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        btnRecepcionista.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    GetVariables.getInstance().setTxtOpcaoFacilities(Enum.Facilities.RECEPCIONISTA.toString());
-                    FacilitiesControleActivity.startActivity(FacilitiesActivity.this);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        btnControladorAcesso.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    GetVariables.getInstance().setTxtOpcaoFacilities(Enum.Facilities.CONTROLADOR.toString());
-                    FacilitiesControleActivity.startActivity(FacilitiesActivity.this);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        btnBombeiro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    GetVariables.getInstance().setTxtOpcaoFacilities(Enum.Facilities.BOMBEIRO.toString());
-                    FacilitiesControleActivity.startActivity(FacilitiesActivity.this);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 
     public static void startActivity(Context from) {
